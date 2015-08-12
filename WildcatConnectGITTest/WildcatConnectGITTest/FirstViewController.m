be6005e7f6c0c5b1210d69488434a6066227c477
wildcatconnectgittest <wildcatconnectgittest@gmail.com>
2015-08-12 09:12:46 -0400

    Added new, refreshed project to repo.

---
+++ WildcatConnectGITTest/WildcatConnectGITTest/FirstViewController.m
@@ -0,0 +1,27 @@
+//
+//  FirstViewController.m
+//  WildcatConnectGITTest
+//
+//  Created by Kevin Lyons on 8/12/15.
+//  Copyright (c) 2015 WildcatConnect. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+@end
