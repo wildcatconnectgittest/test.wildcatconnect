f1166d3da3c4b6981f3bc336bb2c35d31b151121
wildcatconnectgittest <wildcatconnectgittest@gmail.com>
2015-08-12 12:24:15 -0400

    KL - additons.

---
+++ WildcatConnectGITTest/WildcatConnectGITTest/CommunityServiceViewController.h
@@ -0,0 +1,13 @@
+//
+//  CommunityServiceViewController.h
+//  WildcatConnectGITTest
+//
+//  Created by Kevin Lyons on 8/12/15.
+//  Copyright (c) 2015 WildcatConnect. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CommunityServiceViewController : UIViewController
+
+@end
---
+++ WildcatConnectGITTest/WildcatConnectGITTest/Staff DirectoryTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  Staff DirectoryTableViewController.h
+//  WildcatConnectGITTest
+//
+//  Created by Kevin Lyons on 8/12/15.
+//  Copyright (c) 2015 WildcatConnect. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Staff_DirectoryTableViewController : UITableViewController
+
+@end
