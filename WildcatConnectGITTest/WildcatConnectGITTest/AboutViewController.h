068da63285c3e98ed8d79c79f57197598d52ec62
wildcatconnectgittest <wildcatconnectgittest@gmail.com>
2015-08-12 11:21:28 -0400

    PErfect

---
+++ WildcatConnectGITTest/WildcatConnectGITTest/AboutViewController.h
@@ -0,0 +1,15 @@
+//
+//  AboutViewController.h
+//  WildcatConnectGITTest
+//
+//  Created by Rohith Parvathaneni on 8/12/15.
+//  Copyright (c) 2015 WildcatConnect. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController{
+    IBOutlet UIScrollView *scrollerabout;
+}
+
+@end
