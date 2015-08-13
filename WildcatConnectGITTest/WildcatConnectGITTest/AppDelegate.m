b35fea94f246185071e6586e02b9ff0138f384a8
wildcatconnectgittest <wildcatconnectgittest@gmail.com>
2015-08-13 14:12:06 -0400

    Testing database services.

--- WildcatConnectGITTest/WildcatConnectGITTest/AppDelegate.m
+++ WildcatConnectGITTest/WildcatConnectGITTest/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <Parse/Parse.h>
 
 @interface AppDelegate ()
 
@@ -17,6 +18,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 	// Override point for customization after application launch.
+     // Initialize Parse.
+     [Parse setApplicationId:@"cLBOvwh6ZTQYex37DSwxL1Cvg34MMiRWYAB4vqs0"
+                   clientKey:@"jGjp3WuCzf4ZetH8kpTLGNnj1h3DgtHlCuK1QbTi"];
 	return YES;
 }
 
