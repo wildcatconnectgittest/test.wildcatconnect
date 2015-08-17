8dbe1673da8bccfe65124f7a0244252d6a973803
wildcatconnectgittest <wildcatconnectgittest@gmail.com>
2015-08-17 07:24:42 -0400

    Extra...

--- WildcatConnectGITTest/WildcatConnectGITTest/AppDelegate.m
+++ WildcatConnectGITTest/WildcatConnectGITTest/AppDelegate.m
@@ -8,11 +8,18 @@
 
 #import <Foundation/Foundation.h>
 #import "AppDelegate.h"
+#import <Parse/Parse.h>
+#import "ExtracurricularStructure.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+     [Parse setApplicationId:@"cLBOvwh6ZTQYex37DSwxL1Cvg34MMiRWYAB4vqs0"
+                   clientKey:@"jGjp3WuCzf4ZetH8kpTLGNnj1h3DgtHlCuK1QbTi"];
+     ExtracurricularStructure *EC = [[ExtracurricularStructure alloc] init];
+     EC.titleString = @"Test Title...";
+     [EC saveInBackground];
      return YES;
 }
 
