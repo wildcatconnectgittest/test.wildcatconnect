5ee29313e8b2ac528bee3a68dbf400ee6162fdf0
wildcatconnectgittest <wildcatconnectgittest@gmail.com>
2015-08-17 07:10:14 -0400

    Created new AppDelegate.m...

---
+++ WildcatConnectGITTest/WildcatConnectGITTest/AppDelegate.m
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.m
+//  WildcatConnectGITTest
+//
+//  Created by Kevin Lyons on 8/17/15.
+//  Copyright (c) 2015 WildcatConnect. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+     return YES;
+}
+
+@end
